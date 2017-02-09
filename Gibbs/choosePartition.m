beeaeb56f3d57ed7c16a2663c70a4395f0280377
pkielen <pontus.kielen@gmail.com>
2017-02-09 15:03:47 +0100

    changed choosePartition to take in weights instead of hypotheses

--- Gibbs/choosePartition.m
+++ Gibbs/choosePartition.m
@@ -1,16 +1,10 @@
-function  Partition_j  = choosePartition( Hypotheses )
+function  partition  = choosePartition(W_k,partition,point)
 % INPUT: Hypotheses is all the possible partitions when the chosen
 % measurment is placed in each of them
 % OUTPUT: Given the weights, a single partition is chosen from the Hypotheses.
-W_k_hyp=zeros(1,length(Hypotheses));
-
-for i=1:length(Hypotheses)
-    W_k_hyp(i)=Hypotheses{i}.Weight; % Det här är långsamt men svårt att göra utan loop
-end
-
-W_k_norm=W_k_hyp/sum(W_k_hyp);
+W_k_norm=W_k/sum(W_k);
 j = sum(cumsum(W_k_norm)<rand())+1; % pick one cluster at random
+partition.Clusters{j}=partition.Clusters{j}.addPoint(point);
 
-Partition_j=Hypotheses{j};
 end
 
